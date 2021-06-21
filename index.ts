4481dd6557a368f21dd536b3cfe1685d3be645ef
Josh Crites <critesjosh@gmail.com>
2021-06-21 18:07:14 -0400

    add send fx

--- index.ts
+++ index.ts
@@ -3,18 +3,23 @@ import { Provider } from "@ethersproject/providers";
 import { abi } from "./abi/BridgeRouter.json"
 import { BridgeRouter } from "./BridgeRouter";
 
-const alfajoresBridgeAddress: string = ""
-const kovanBridgeAddress: string = ""
 let tokenBridgeRouter: BridgeRouter
 let chainId: Number
-const alfajoresCeloAddress: string = "0xE51e1032Ff1D6CcC8152d53e9f0661aEdE2D98F8"
+
+const alfajoresBridgeAddress: string = "0x0000000000000000000000000000000000000000"
+const kovanBridgeAddress: string     = "0x0000000000000000000000000000000000000000"
+const alfajoresCeloAddress: string   = "0xE51e1032Ff1D6CcC8152d53e9f0661aEdE2D98F8"
+const alfajorescETHAddress: string   = "0x0000000000000000000000000000000000000000"
+const kovaneCeloAddress: string      = "0x0000000000000000000000000000000000000000"
+const kovanWETHAddress: string       = "0x0000000000000000000000000000000000000000"
+
 const alfajoresDomain: ethers.BigNumberish = 1000
 const kovanDomain: ethers.BigNumberish = 3000
+
 const alfajoresChainId = 44787
 const kovanChainId = 42
 
 const recipient: string = "0x9e643F570FAcB7Be198aC287d4926b18528b6E89" // can be whatever
-const amountToSend: ethers.BigNumberish = ethers.utils.parseEther("0.01")
 
 const connectWallet = async function () {
     //@ts-ignore
@@ -44,28 +49,60 @@ const connectWallet = async function () {
 
     // if the network is alfajores
     // - allow users to send CELO to kovan
-    async function sendCeloToKovan(){
-        if(chainId != alfajoresChainId) throw "must be on the celo alfajores network"
-        tokenBridgeRouter.send(alfajoresCeloAddress, kovanDomain, recipient, amountToSend)
-    }
+    async function sendCeloToKovan(_amountToSend: ethers.BigNumberish){}
     // - allow users to send cETH to another alfajores account
     // NEED: what is the cETH contract address?
-    async function sendcETH(){
-    }
+    async function sendcETH(){}
     // - allow users to send cETH back to kovan
     // NEED: what is the cETH contract address?
-    async function sendcETHToKovan(){
-    }
+    async function sendcETHToKovan(){}
 
     // if the network is kovan
     // - allow users to send ETH to alfajores
     // - allow users to send eCELO to another kovan account
     // - allow users to send eCELO back to alfajores
+async function send(_fx: string, _amountToSend: ethers.BigNumberish){
+    let tx
 
+    if(chainId == alfajoresChainId){
+        switch (_fx) {
+            case "sendXchain":
+                tx = await tokenBridgeRouter.send(alfajoresCeloAddress, kovanDomain, recipient, _amountToSend)
+            case "send":
+                tx = await tokenBridgeRouter.send(alfajorescETHAddress, alfajoresDomain, recipient, _amountToSend)
+            case "sendHome":
+                tx = await tokenBridgeRouter.send(alfajorescETHAddress, kovanDomain, recipient, _amountToSend)
+        }
+    } else if (chainId == kovanChainId){
+        switch (_fx) {
+            case "sendXchain":
+                tx = await tokenBridgeRouter.send(kovanWETHAddress, alfajoresDomain, recipient, _amountToSend)
+            case "send":
+                tx = await tokenBridgeRouter.send(kovaneCeloAddress, kovanDomain, recipient, _amountToSend)
+            case "sendHome":
+                tx = await tokenBridgeRouter.send(kovaneCeloAddress, alfajoresDomain, recipient, _amountToSend)
+        }
+    } else {
+        throw "please select the Kovan or Alfajores network"
+    }
+
+    console.log(tx)
+}
 
 //@ts-ignore
 document.querySelector("#login").addEventListener("click", async (e) => {
     connectWallet()
+})
+
+//@ts-ignore
+document.querySelector("#sendTx").addEventListener("click", async (e) => {
+    //@ts-ignore
+    let amountToSend = ethers.utils.parseEther(document.querySelector("#amount").value)
+    //@ts-ignore
+    let fx = document.querySelector("#action").value
+
+    console.log(fx, amountToSend)
+    // send(fx, amountToSend)
 })
 
 function setNetworkHtml(chainId: Number){
